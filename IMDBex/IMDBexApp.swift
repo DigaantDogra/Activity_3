7938a192666f6af7235c17adcd8abb784f9777c5
Digaant Dogra <dogra057@gmail.com>
2025-01-09 09:33:15 -0500

    Initial Commit

---
+++ IMDBex/IMDBexApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDBexApp.swift
+//  IMDBex
+//
+//  Created by Digaant Dogra on 2025-01-09.
+//
+
+import SwiftUI
+
+@main
+struct IMDBexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
